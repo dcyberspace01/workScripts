224c1cd0ee7e8ad6ceb2dc76d5f1e92215b6b220
Digit <152463773+dcyberspace01@users.noreply.github.com>
2025-10-08 14:43:17 -0500

    Update win24h2Upgrade.ps1 download url

--- win24h2Upgrade.ps1
+++ win24h2Upgrade.ps1
@@ -3,7 +3,7 @@ function Start-Windows11Upgrade {
     #URL will only last for 24 Hours. To get a URL you need to go to the Windows Site input a Language and Right Click the Download Box and Copy Address/Copy Link Address (Chrome)
 
     param (
-        [string]$IsoUrl = "https://software.download.prss.microsoft.com/dbazure/Win11_25H2_English_x64.iso?t=96b4a81f-b724-4a5a-96c9-7741a00b52af&P1=1759931976&P2=601&P3=2&P4=yA7hT8QpB0w7tHLBFeF6BudzBjGGbS9rYH8zT6Be%2fhM%2bIB%2bsLqkQz5gADp2B1%2bvv7mQhS4TIcPQGrDo5G6HXEm5P8CmKK%2bx8JBvHDbdN6H%2bds1S7EeKJX9FGKUyBWV0zldn0I%2bHL6ZW0KPALAxKsstmVGetzAbbK1qJCMpO5vMnhUGu4DshHy4XmCBP9r%2bZPZ%2fid4YgxM2yhX7IhYsv79qyFiNu66jkaixnPsdyFDWnOSYeYUxEfzmGYzfLzHrydZey22TYMKIIB3rQzgKiNU7gr1zzYMqW1BS4WmeBb1Ns0G2SKnJCks%2bVS%2bLaIOTX1zhEaEuAudCXwwtU9a4%2fn0w%3d%3d",
+        [string]$IsoUrl = "https://software.download.prss.microsoft.com/dbazure/Win11_25H2_English_x64.iso?t=4d4931eb-cc02-4e81-bc78-27ded91b1358&P1=1760038940&P2=601&P3=2&P4=GjCwX%2bjQkMCz%2bmi5wP0V54RlkCd60Jb4ulyOMGQkaXtppd5doD0eYCnDUKlYQIRVVK8ywYJ%2fGAYbS8aBTZv2fqPYO4zASaMlZjNe5t9WYrNQ4d%2fgSTNDKob8ZH89jI78Wztv8lk2%2fhV7fWfTfS8SEoeGjCPkukqzsiZDUkZ%2bHz1YAce4grHi6k3xwcpxgT46fVnQi2AakzqTTzv8m9gwr8j8Cado4SgsHzWoh5PjvrojzqFf8V0dmau1OmEbScKA5hTaJ98LuYb11bx2QksYP0p4INi1iR2t9xM6VclOQVC0vSDT8kVSTSSf4KbbrQ9Eyashjv5bVqiA4V45a02DBg%3d%3d",
         [string]$IsoPath = "C:\Temp\windows11.iso"
     )
 
